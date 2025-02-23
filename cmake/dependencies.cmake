347376bab829d73163d124d7e8855ba26e15d075
Drunk Fly Team <drunkflyteam@gmail.com>
2025-02-23 19:56:18 +0200

    Don't build Lua interptereter for cross-compiling by default.

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,10 +1,2 @@
 # This file is executed by buildtool before building the project
 # Use it to setup the environment and compile additional tools if needed
-
-if(CROSSCOMPILING)
-    build_host_tool(lua
-        DIRECTORY "${CURRENT_PROJECT_DIR}"
-        TARGETS LuaInterpreter LuaCompiler
-        EXECUTABLES lua luac
-        )
-endif()
